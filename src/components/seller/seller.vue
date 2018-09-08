683d0d176e8fc5ab1aa58de420c77cb865949150
王开波 <568966289@qq.com>
2018-09-09 00:39:19 +0800

    完成商家页开发

--- src/components/seller/seller.vue
+++ src/components/seller/seller.vue
@@ -34,6 +34,37 @@
         </div>
       </div>
       <split></split>
+      <div class="bulletin">
+        <div class="title">公告与活动</div>
+        <div class="content-wrapper border-1px">
+          <p class="content">{{seller.bulletin}}</p>
+        </div>
+        <!--优惠信息列表-->
+        <ul v-if="seller.supports" class="supports">
+          <li class="support-item border-1px" v-for="(item,index) in seller.supports" :key="index">
+            <span class="icon" :class="classMap[item.type]"></span>
+            <span class="text">{{item.description}}</span>
+          </li>
+        </ul>
+      </div>
+      <split></split>
+      <div class="pics">
+        <h1 class="title">商家实景</h1>
+        <div class="pic-wrapper" ref="picWrapper">
+          <ul class="pic-list" ref="picList">
+            <li class="pic-item" v-for="(pic,index) of seller.pics" :key="index">
+              <img :src="pic" width="120" height="90"/>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <split></split>
+      <div class="info">
+        <h1 class="title border-1px">商家信息</h1>
+        <ul>
+          <li class="info-item" v-for="info in seller.infos" :key="info.key">{{info}}</li>
+        </ul>
+      </div>
     </div>
   </div>
 </template>
@@ -54,6 +85,7 @@ export default {
   },
   data() {
     return {
+      classMap: ['decrease', 'discount', 'special', 'invoice', 'guarantee'],
       // loadFromLocal从本地获取上次保存的值
       favorite: (() => loadFromLocal(this.seller.id, 'favorite', false))(),
       favoriteText: '收藏'
@@ -61,9 +93,8 @@ export default {
   },
   watch: {
     'seller'() {
-      this.$nextTick(() => {
-        this._initScroll()
-      })
+      this._initScroll()
+      this._initPics()
     },
     'favorite'() {
       this.favorite ? this.favoriteText = '已收藏' : this.favoriteText = '收藏'
@@ -80,7 +111,7 @@ export default {
       // 持久化到本地
       saveToLocal(this.seller.id, 'favorite', this.favorite)
     },
-    // 初始化滚动
+    // 初始化纵向滚动
     _initScroll() {
       if (!this.scroll) {
         this.scroll = new BScroll(this.$refs.seller, {
@@ -89,7 +120,26 @@ export default {
       } else {
         this.scroll.refresh()
       }
-    }
+    },
+    // 初始化横向滚动
+    _initPics() {
+      if (this.seller.pics) {
+        let picWidth = 120
+        let margin = 6
+        let width = (picWidth + margin) * this.seller.pics.length - margin
+        this.$refs.picList.style.width = width + 'px'
+        this.$nextTick(() => {
+          if (!this.picScroll) {
+            this.picScroll = new BScroll(this.$refs.picWrapper, {
+              scrollX: true,
+              eventPassthrough: 'vertical'
+            })
+          } else {
+            this.scroll.refresh()
+          }
+        })
+      }
+    },
   },
   created() {
   },
@@ -169,4 +219,82 @@ export default {
           line-height 10px
           font-size 10px
           color rgb(77, 85, 93)
+    .bulletin
+      padding 18px 18px 0 18px
+      .title
+        margin-bottom 8px
+        line-height 14px
+        color rgb(7, 17, 27)
+        font-size 14px
+      .content-wrapper
+        padding 0 12px 16px 12px
+        border-1px(rgba(7, 17, 27, 0.1))
+        .content
+          line-height 24px
+          font-size 12px
+          color rgb(240, 20, 20)
+      .supports
+        .support-item
+          padding 16px 12px
+          font-size 0
+          border-1px(rgba(7, 17, 27, 0.1))
+          &:last-child
+            margin-bottom 0
+          .icon
+            display inline-block
+            width 16px
+            height 16px
+            vertical-align top
+            margin-right 6px
+            background-size 16px 16px
+            background-repeat no-repeat
+            &.decrease
+              bg-image('decrease_4')
+            &.discount
+              bg-image('discount_4')
+            &.guarantee
+              bg-image('guarantee_4')
+            &.invoice
+              bg-image('invoice_4')
+            &.special
+              bg-image('special_4')
+          .text
+            line-height 16px
+            font-size 12px
+    .pics
+      padding 18px
+      .title
+        margin-bottom 12px
+        line-height 14px
+        color rgb(7, 17, 27)
+        font-size 14px
+      .pic-wrapper
+        width 100%
+        overflow hidden
+        white-space nowrap
+        .pic-list
+          font-size 0
+          .pic-item
+            display inline-block
+            margin-right 6px
+            width 120px
+            height 90px
+            &.last-child
+              margin 0
+    .info
+      padding: 18px 18px 0 18px
+      color: rgb(7, 17, 27)
+      .title
+        padding-bottom: 12px
+        line-height: 14px
+        color: rgb(7, 17, 27)
+        font-size: 14px
+        border-1px(rgba(7, 17, 27, 0.1))
+      .info-item
+        padding: 16px 12px
+        line-height: 16px
+        border-1px(rgba(7, 17, 27, 0.1))
+        font-size: 12px
+        &:last-child
+          border-none()
 </style>
